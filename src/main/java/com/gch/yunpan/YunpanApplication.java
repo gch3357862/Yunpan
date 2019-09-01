5b52a37ef8d089cdabfd592fef7ec8005f6e1d6c
gch3357862 <gch3357862@qq.com>
2019-09-01 12:19:28 +0800

    初始化项目

---
+++ src/main/java/com/gch/yunpan/YunpanApplication.java
@@ -0,0 +1,13 @@
+package com.gch.yunpan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunpanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YunpanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gch/yunpan/YunpanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gch.yunpan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YunpanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
