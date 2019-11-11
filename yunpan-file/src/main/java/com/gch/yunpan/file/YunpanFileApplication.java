1fafbe73cc4a7498a3fb8ac557df06c90227d49c
guch <sx-guch@dtdream.com>
2019-11-11 13:43:17 +0800

    20191111

--- yunpan-file/src/main/java/com/gch/yunpan/file/YunpanFileApplication.java
+++ yunpan-file/src/main/java/com/gch/yunpan/file/YunpanFileApplication.java
@@ -3,9 +3,11 @@ package com.gch.yunpan.file;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.DependsOn;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.gch.yunpan.file.mapper")
+@DependsOn("flywayInitializer")
 public class YunpanFileApplication {
 
 	public static void main(String[] args) {
--- yunpan-user/src/main/java/com/gch/yunpan/user/YunpanUserApplication.java
+++ yunpan-user/src/main/java/com/gch/yunpan/user/YunpanUserApplication.java
@@ -3,9 +3,11 @@ package com.gch.yunpan.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.DependsOn;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.gch.yunpan.user.mapper")
+@DependsOn("flywayInitializer")
 public class YunpanUserApplication {
 
 	public static void main(String[] args) {
